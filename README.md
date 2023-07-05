c5e15331267ab98611a3f8dd5e69a964428f4c76
Mr Honey Bunny <134079876+schooldropout1337@users.noreply.github.com>
2023-07-05 12:23:34 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,57 @@
+# Dork Tool
+
+Dork Tool is a Python script that allows you to perform Google dorking, which is a method of using advanced search operators to find specific information on the web using the Google search engine.
+
+## Prerequisites
+
+- Python 3.x
+- `pip` package manager
+
+## Installation
+
+1. Clone the repository:
+
+   ```shell
+   git clone https://github.com/xyz/dork.git
+   ```
+
+2. Navigate to the cloned directory:
+
+   ```shell
+   cd dork
+   ```
+
+3. Install the required dependencies:
+
+   ```shell
+   pip install -r requirements.txt
+   ```
+
+## Usage
+
+1. Run the script using the following command:
+
+   ```shell
+   python dork.py -d <search_query>
+   ```
+
+Replace `<search_query>` with your desired search query. Enclose the query in quotes if it contains spaces or special characters.
+
+2. The script will retrieve the search results from Google and extract the URLs matching the search query.
+
+3. The extracted URLs will be printed to the console.
+
+## Note: VPN Usage
+
+If you encounter rate limits or IP address blocking from Google during usage, it is recommended to use a VPN (Virtual Private Network) service. A VPN can help you mask your IP address and bypass any restrictions set by Google.
+
+Ensure that you have a reliable VPN service installed and running before using the Dork Tool. This will help you avoid any potential limitations and ensure smooth execution.
+
+## Disclaimer
+
+Please use this tool responsibly and adhere to the terms of service of the search engine you are using. The Dork Tool is intended for educational and informational purposes only. The authors are not responsible for any misuse or illegal activities performed using this tool.
+
+## License
+
+This project is licensed to dork.
+
---
+++ dork.py
@@ -0,0 +1,74 @@
+import argparse
+import requests
+import bs4
+import urllib.parse
+import re
+
+
+print('''\033[92m
+     _            _
+  __| | ___  _ __| | __
+ / _` |/ _ \| '__| |/ /
+| (_| | (_) | |  |   <
+ \__,_|\___/|_|  |_|\_/
+
+    \033[00m''')
+
+# Create an argument parser
+parser = argparse.ArgumentParser(description='Google Dork Script')
+
+# Add the input argument
+parser.add_argument('-d', '--dork', type=str, help='Specify the search dork.')
+
+# Parse the command line arguments
+args = parser.parse_args()
+
+# Check if the dork argument is provided
+if not args.dork:
+    parser.error('The search query (-d/--dork) is required.')
+
+# Retrieve the search query from the command line arguments
+dork = args.dork
+
+# Convert the search query to URL-encoded format
+encoded_query = urllib.parse.quote(dork)
+#print(encoded_query)
+print("dork : " '\033[93m' + dork + '\033[00m\n')
+
+
+# Define the parameters to remove
+params_to_remove = ['sa', 'ved', 'usg']
+
+# Iterate over the first three pages of search results
+for page in range(1):
+    # Calculate the start index for the current page
+    start = page * 10
+
+    # Construct the Google search URL for the current page
+    #url = f'https://google.com/search?q={dork}&start={start}'
+    url = f'https://google.com/search?q={dork}'
+
+    # Send a GET request to the URL and store the response
+    request_result = requests.get(url)
+
+    # Create a BeautifulSoup object from the response HTML
+    soup = bs4.BeautifulSoup(request_result.text, "html.parser")
+
+    # Find all anchor tags (links) within the search results
+    link_tags = soup.find_all('a')
+
+    # Iterate through the link tags and extract the URLs
+    for link_tag in link_tags:
+        href = link_tag.get('href')
+        if href.startswith('/url?q='):  # Extract the URL from the href attribute
+            url = href[7:]  # Remove the '/url?q=' prefix
+            url = urllib.parse.unquote(url)  # Decode URL-encoded characters
+
+            if not (url.startswith('https://www.google.com') or
+                    url.startswith('https://support.google.com/web') or
+                    url.startswith('https://accounts.google.com')):  # Exclude specified links
+                # Remove the parameters along with their values
+                cleaned_url = re.sub(r'&?(?:{})=.*?(?=&|$)'.format('|'.join(params_to_remove)), '', url)
+                print(cleaned_url)
+
+print()
